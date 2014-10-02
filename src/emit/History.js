442c3d8d77da5559be3f14624e5c2317eb3490e1
Alex Kit <source.kit@gmail.com>
2014-10-02 02:15:09 +0200

    fix / HistoryAPI. Reset initial state on `navigate`

--- src/emit/History.js
+++ src/emit/History.js
@@ -1,51 +1,40 @@
-
-function HistoryEmitter(listener){
-	
+function HistoryEmitter(listener){	
 	if (typeof window === 'undefined')
 		return null;
 	
 	if (!(window.history && window.history.pushState))
 		return null;
 
-	var that = this;	
-	
+	var that = this;
 	that.listener = listener;
 	that.initial = location.pathname;
-	
 	
 	window.onpopstate = function(){
 		if (that.initial === location.pathname) {
 			that.initial = null;
 			return;
 		}
-		
 		that.changed();
 	};
 	
 	return that;
 }
 
-(function(){
-	
-	HistoryEmitter.prototype = {
-		navigate: function(url){
-			if (url == null) {
-				this.changed();
-				return;
-			}
-			
-			history.pushState({}, null, url);
+HistoryEmitter.prototype = {
+	navigate: function(url){
+		if (url == null) {
 			this.changed();
-		},
-		
-		changed: function(){
-			
-			this.listener.changed(location.pathname + location.search);
-		},
-		current: function(){
-			
-			return location.pathname + location.search;
+			return;
 		}
-	};
-
-}());
+		
+		history.pushState({}, null, url);
+		this.initial = null;
+		this.changed();
+	},
+	changed: function(){
+		this.listener.changed(location.pathname + location.search);
+	},
+	current: function(){
+		return location.pathname + location.search;
+	}
+};

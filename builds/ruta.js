0381571bd574f60910d9abee080809290f61e8d8
Alex Kit <source.kit@gmail.com>
2015-07-19 22:47:34 +0200

    add (npmignore)

--- builds/ruta.js
+++ builds/ruta.js
@@ -1,20 +1,28 @@
 (function(root, factory){
 	"use strict";
-	
-	if (root == null) {
-		root = typeof window !== 'undefined' && typeof document !== 'undefined' 
-			? window 
-			: global;
+
+	var isNode = (typeof window === 'undefined' || window.navigator == null);
+	var global_ = isNode ? global : window;
+
+	function construct(){
+		var ruta = factory(global_);
+		if (isNode) {
+			module.exports = ruta;
+			return;
+		}
+		return window.ruta = ruta;
 	}
-	
-	
-	root.ruta = factory(root);
-	
+
+	if (typeof define === 'function' && define.amd) {
+		return define(construct);
+	}
+
+	return construct();
 }(this, function(global){
 	"use strict";
-	
+
 	// import ../src/vars.js
-	
+
 	// import ../src/utils/obj.js
 	// import ../src/utils/log.js
 	// import ../src/utils/path.js
@@ -25,11 +33,11 @@
 	// import ../src/route/Collection.js
 
 	// import ../src/emit/Location.js
-	
+
 	// import ../src/api/utils.js
 	// import ../src/ruta.js
-	
+
 	// import ../src/mask/attr/anchor-dynamic.js
-	
+
 	return Ruta;
 }));

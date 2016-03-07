efd0ea0a15596300412fc6ee4d620117dbc5eb6b
Alex Kit <source.kit@gmail.com>
2016-03-07 10:05:13 +0100

    fix (DynamicA) get attribute instead of href property

--- src/mask/attr/anchor-dynamic.js
+++ src/mask/attr/anchor-dynamic.js
@@ -2,15 +2,15 @@
 	if (mask == null) {
 		return;
 	}
-	
+
 	mask.registerAttrHandler('x-dynamic', function(node, value, model, ctx, tag){
 		tag.onclick = navigate;
 	}, 'client');
-	
+
 	function navigate(event) {
 		event.preventDefault();
 		event.stopPropagation();
-		
-		Ruta.navigate(this.href);
+
+		Ruta.navigate(this.getAttribute('href'));
 	}
 }());

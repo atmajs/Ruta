80ceebaccb230d51dc23956d62199d25866e5cbd
Alex Kit <source.kit@gmail.com>
2017-09-01 16:48:21 +0200

    update (utest)

--- test/config.js
+++ test/config.js
@@ -1,4 +1,27 @@
 module.exports = {
-	env: '/lib/ruta.js',
-	tests: ['/test/**.test']
+	$config: {
+		$before () {
+			process.on('unhandledRejection', (...args) => {
+				console.log(args);
+			});
+			process.on('uncaughtException', (...args) => {
+				console.log(args);
+			});
+
+			include
+				.cfg('extentionDefault', { js: 'ts' })
+				.cfg('amd', true);
+		}	
+	},
+	suites: {
+		dom: {
+			env: '/lib/ruta.js',
+			tests: ['/test/dom/**.test']
+		},
+		node: {
+			env: '/lib/ruta.js',
+			tests: ['/test/node/**.test']
+		}
+	}
+	
 };

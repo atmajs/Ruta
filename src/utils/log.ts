b7fe7399ab01ba9370f1b9173d165f62d7447d45
Alex Kit <source.kit@gmail.com>
2017-09-04 09:20:22 +0200

    v0.2.37

--- src/utils/log.ts
+++
@@ -1,3 +0,0 @@
-export function log_error (...args: any[]){	
-	console.error('Ruta', ...args);
-};
--- typings/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="globals/assertion/index.d.ts" />
-/// <reference path="globals/atma-utest/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
